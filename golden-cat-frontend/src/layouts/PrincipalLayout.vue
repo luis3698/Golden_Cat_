12b3b523e253ac8745b51fe0172a71a1b30a1d42
luis3698 <gambis3698@gmail.com>
2019-03-19 16:02:02 -0500

    golden_cat_inicio

--- golden-cat-frontend/src/layouts/PrincipalLayout.vue
+++ golden-cat-frontend/src/layouts/PrincipalLayout.vue
@@ -1,117 +1,48 @@
 <template>
-   <div id="app">
-  <v-app
-    dark
-    id="inspire"
-  >
-    <v-navigation-drawer
-      fixed
-      clipped
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-tile v-for="item in items" :key="item.text" @click="">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ item.text }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
-        <v-list>
-          <v-list-tile v-for="item in items2" :key="item.text" avatar @click="">
-            <v-list-tile-avatar>
-              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
-            </v-list-tile-avatar>
-            <v-list-tile-title v-text="item.text"></v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-        <v-list-tile class="mt-3" @click="">
-          <v-list-tile-action>
-            <v-icon color="grey darken-1">add_circle_outline</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title class="grey--text text--darken-1">Browse Channels</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon color="grey darken-1">settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      color="red"
-      dense
-      fixed
-      clipped-left
-      app
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-icon class="mx-3">fa-youtube</v-icon>
-      <v-toolbar-title class="mr-5 align-center">
-        <span class="title">Golden Cat</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-layout row align-center style="max-width: 650px">
-        <v-text-field
-          placeholder="Search..."
-          single-line
-          append-icon="search"
-          :append-icon-cb="() => {}"
-          color="white"
-          hide-details
-        ></v-text-field>
+  <v-app  id="inspire" dark>
+    <v-toolbar color="indigo darken-4" app fixed clipped-left>
+      <v-layout row wrap>
+        <v-flex xs3>
+          <v-img height="65px" :src="logo" ></v-img>
+        </v-flex>
       </v-layout>
+
+      <!-- <v-toolbar-title  left>Hotel Golden Cat </v-toolbar-title > -->
+
+        <v-spacer></v-spacer>
+         <v-btn  flat>Inicio
+          <v-icon>business</v-icon>
+        </v-btn>
+        <v-btn flat>Servicios extra
+          <v-icon>bookmarks</v-icon>
+        </v-btn>
+        <v-btn flat>Contactanos
+          <v-icon>email</v-icon>
+        </v-btn>
+        <v-btn flat>¿Que es golden cat
+          <v-icon >help</v-icon>
+        </v-btn>
+        <v-btn flat>Crear cuenta
+          <v-icon >account_circle</v-icon>
+        </v-btn>
     </v-toolbar>
-    <v-content>
-      <v-container fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-tooltip right>
-              <v-btn
-                icon
-                large
-                :href="source"
-                target="_blank"
-                slot="activator"
-              >
-                <v-icon large>code</v-icon>
-              </v-btn>
-              <span>Source</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+     <v-toolbar-title  left>Hotel Golden Cat </v-toolbar-title >
   </v-app>
-</div>
-    
+
 </template>
+
 <script>
+import Logo from '@/assets/golden_cat.png'
 export default {
-    data() {
-        return {
-            drawer: true,
-      items: [
-        { icon: 'trending_up', text: 'Most Popular' },
-        { icon: 'subscriptions', text: 'Subscriptions' },
-        { icon: 'history', text: 'History' },
-        { icon: 'featured_play_list', text: 'Playlists' },
-        { icon: 'watch_later', text: 'Watch Later' }
-      ],
-      items2: [
-        { picture: 28, text: 'Joseph' },
-        { picture: 38, text: 'Apple' },
-        { picture: 48, text: 'Xbox Ahoy' },
-        { picture: 58, text: 'SEBITAS' },
-        { picture: 78, text: 'MKBHD' }
-      ]
-        }
+  data() {
+    return {
+      logo: Logo
     }
+  }
 }
+
 </script>
+
+<style>
+
+</style>
