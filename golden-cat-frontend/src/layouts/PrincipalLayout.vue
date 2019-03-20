52f51534d7713a3687dc1c79d7d15f669d9bf60d
luis3698 <gambis3698@gmail.com>
2019-03-20 12:53:09 -0500

    golden_cat

--- golden-cat-frontend/src/layouts/PrincipalLayout.vue
+++ golden-cat-frontend/src/layouts/PrincipalLayout.vue
@@ -1,49 +1,75 @@
+
 <template>
-  <v-app  id="inspire" dark>
-    <v-toolbar color="indigo darken-4" app fixed clipped-left>
-      <v-layout row wrap>
-        <v-flex xs3>
-          <v-img height="65px" :src="logo" ></v-img>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      right
+      app
+    >
+      <v-list dense>
+        <v-list-tile  @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>inciar sesion</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>crear cuenta</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="cyan" dark fixed app>
+      <v-spacer></v-spacer>
+        <v-layout row wrap>
+        <v-flex xs2>
+           <v-img src="@/assets/logo.png" ></v-img>
         </v-flex>
       </v-layout>
 
-      <!-- <v-toolbar-title  left>Hotel Golden Cat </v-toolbar-title > -->
-
-        <v-spacer></v-spacer>
-         <v-btn  flat>Inicio
+        <v-spacer ></v-spacer>
+         <v-btn small flat >inicio
           <v-icon>business</v-icon>
         </v-btn>
-        <v-btn flat>Servicios extra
+        <v-btn small flat>Servicios extra
           <v-icon>bookmarks</v-icon>
         </v-btn>
-        <v-btn flat>Contactanos
+        <v-btn small flat>Contactanos
           <v-icon>email</v-icon>
         </v-btn>
-        <v-btn flat>¿Que es golden cat
-          <v-icon >help</v-icon>
+        <v-btn small flat>¿que es golden cat?
+          <v-icon>help</v-icon>
         </v-btn>
-        <v-btn flat>Cuenta
-          <v-icon >account_circle</v-icon>
-        </v-btn>
+      <v-btn  flat @click.stop="drawer = !drawer">cuenta
+      <v-icon>account_circle</v-icon>
+      </v-btn>
     </v-toolbar>
-     <v-toolbar-title  left>Hotel Golden Cat </v-toolbar-title >
-     
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
-
 </template>
 
+
 <script>
-import Logo from '@/assets/golden_cat.png'
 export default {
-  data () {
-    return {
-      logo: Logo
-    }
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
   }
 }
 
+
 </script>
-
-<style>
-
-</style>
