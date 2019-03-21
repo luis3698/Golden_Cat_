42c968115e22b92c62e151a35a3ab19c2f0c2976
sebas2125 <jsmancillla@misena.edu.co>
2019-03-21 13:57:09 -0500

    ordenacion_2

--- golden-cat-frontend/src/router.js
+++ golden-cat-frontend/src/router.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Registro from './views/Registro.vue'
-import Contactos from './views/Contactos.vue'
-import Servicios from './views/Servicios.vue'
+import Registro from './views/Principal/Registro.vue'
+import Contactos from './views/Principla/Contactos.vue'
+import Servicios from './views/Principal/Servicios.vue'
 
-import RestaurarContrasena from './views/restaurar_contraseña.vue'
-import Inicio from './views/Inicio.vue'
-import Reservas from './views/Reservas.vue'
-import QueEsGoldenCat from './views/Que_es_golden_cat.vue'
+import RestaurarContrasena from './views/Principal/restaurar_contraseña.vue'
+import Inicio from './views/Principal/Inicio.vue'
+import Reservas from './views/Administrador/Reservas.vue'
+import QueEsGoldenCat from './views/Principal/Que_es_golden_cat.vue'
 
 Vue.use(Router)
 
