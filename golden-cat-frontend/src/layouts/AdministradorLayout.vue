2e594960c647c55e8627666268553454ef848820
Maicol Sanchez <mssanchez908@misena.edu.co>
2019-03-22 10:05:51 -0500

    HLM

--- golden-cat-frontend/src/layouts/AdministradorLayout.vue
+++ golden-cat-frontend/src/layouts/AdministradorLayout.vue
@@ -102,20 +102,14 @@
               <v-list>
                 <v-list-tile avatar>
                   <v-list-tile-avatar>
-                    <img src="../assets/golden_cat.png" alt="John">
+                    <img src="../assets/golden_cat.png" alt="">
                   </v-list-tile-avatar>
                   <v-list-tile-content>
-                    <v-list-tile-title>John Leider</v-list-tile-title>
-                    <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+                    <v-list-tile-title>Administrador</v-list-tile-title>
+                    <v-list-tile-sub-title>Michaell Sanchez</v-list-tile-sub-title>
                   </v-list-tile-content>
                   <v-list-tile-action>
-                    <v-btn
-                      :class="fav ? 'red--text' : ''"
-                      icon
-                      @click="fav = !fav"
-                    >
-                      <v-icon>favorite</v-icon>
-                    </v-btn>
+                   
                   </v-list-tile-action>
                 </v-list-tile>
               </v-list>
@@ -125,19 +119,14 @@
                   <v-list-tile-action>
                     <v-switch v-model="message" color="purple"></v-switch>
                   </v-list-tile-action>
-                  <v-list-tile-title>Enable messages</v-list-tile-title>
+                  <v-list-tile-title>Mensajes activos</v-list-tile-title>
                 </v-list-tile>
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-switch v-model="hints" color="purple"></v-switch>
-                  </v-list-tile-action>
-                  <v-list-tile-title>Enable hints</v-list-tile-title>
-                </v-list-tile>
+                
               </v-list>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn flat @click="menu = false">Cancel</v-btn>
-                <v-btn color="primary" flat @click="menu = false">Save</v-btn>
+                <v-btn flat @click="menu = false">Cancelar</v-btn>
+                <v-btn color="primary" flat @click="menu = false">Guardar</v-btn>
               </v-card-actions>
             </v-card>
           </v-menu>
