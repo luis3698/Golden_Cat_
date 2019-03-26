30124852351f53bc639e6b8ed0f94cd53bac3b23
sebas2125 <jsmancillla@misena.edu.co>
2019-03-26 15:31:13 -0500

    inicio

--- golden-cat-frontend/src/views/Principal/Inicio.vue
+++ golden-cat-frontend/src/views/Principal/Inicio.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>Encuentra tu Habitacion</h1>
-    <v-layout wrap row>
+    <v-layout wrap justify-space-around row fill-height>
       <v-flex xs12 sm6 md3>
-        <v-text-field label="Fecha de Entrada" outline ></v-text-field>
+        <v-text-field label="Fecha de Entrada" outline color: black></v-text-field>
       </v-flex>
        <v-flex xs12 sm6 md3>
         <v-text-field label="Fecha de Salida" outline></v-text-field>
@@ -14,6 +14,222 @@
        <v-flex xs12 sm6 md3>
         <v-text-field label="Numero de Niños" outline></v-text-field>
       </v-flex>
+
+        <v-flex xs12 sm3>
+          <v-container fill-height>
+          <v-card >
+
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container >
+        </v-flex>
+
+        <v-flex xs12 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+
+        <v-flex xs12 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+
+        <v-flex xs16 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+
+        <v-flex xs16 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+        <v-flex xs16 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+
+        <v-flex xs16 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
+
+        <v-flex xs16 sm3>
+          <v-container fill-height>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">Top western road trips</div>
+                <span class="grey--text">1,000 miles of wonder</span>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat>Share</v-btn>
+              <v-btn flat color="purple">Explore</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-slide-y-transition>
+              <v-card-text v-show="show">
+                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+              </v-card-text>
+            </v-slide-y-transition>
+          </v-card>
+          </v-container>
+        </v-flex>
     </v-layout>
   </div>
 </template>
@@ -21,12 +237,12 @@
 export default {
   created () {
     this.$store.commit('SET_LAYOUT', 'principal-layout')
-  }
+  },
+  data: () => ({
+    show: false
+  })
 }
 </script>
 <style>
-.v-text-field--outline > .v-input__control > .v-input__slot
-{
-  background-color: white !important
-}
+
 </style>
