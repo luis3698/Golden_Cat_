ac3843f96251d86e5b6939e93a2d735f0d568518
luis3698 <gambis3698@gmail.com>
2019-04-01 09:22:32 -0500

    actual

--- golden-cat-frontend/src/views/Principal/Reservas_Cliente.vue
+++ golden-cat-frontend/src/views/Principal/Reservas_Cliente.vue
@@ -1,18 +1,16 @@
 <template>
-    <h1>hola mundo</h1>
+
 </template>
 
 <script>
 export default {
-    name: 'inicio',
-    created () {
-        this.$store.commit('SET_LAYOUT', 'principal-layout')
-  },
+  name: 'inicio',
+  created () {
+    this.$store.commit('SET_LAYOUT', 'principal-layout')
+  }
 }
 </script>
 
 <style>
 
 </style>
-
-
