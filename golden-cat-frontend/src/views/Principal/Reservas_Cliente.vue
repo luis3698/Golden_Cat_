3204cfcbed7ee74fab55bd4dd37a4067cfc97986
luis3698 <gambis3698@gmail.com>
2019-04-02 15:46:41 -0500

    sdsf

--- golden-cat-frontend/src/views/Principal/Reservas_Cliente.vue
+++ golden-cat-frontend/src/views/Principal/Reservas_Cliente.vue
@@ -1,6 +1,34 @@
 <template>
+      <v-tabs v-model="active" color="cyan" dark  slider-color="yellow" >
+        <v-tab v-for="n in 3" :key="n" ripple  >
+          Item {{ n }}
+        </v-tab>
+
+        <v-tab-item v-for="n in 3" :key="n">
+          <v-card flat>
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab-item v-for="n in 3" :key="n">
+          <v-card flat>
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab-item v-for="n in 3" :key="n">
+          <v-card flat>
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+
+      
 
 </template>
+
+
+
 
 <script>
 export default {
@@ -10,7 +38,7 @@ export default {
   }
 }
 </script>
-
+  
 <style>
 
 </style>
