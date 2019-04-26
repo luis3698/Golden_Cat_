e1f28f1e6f69a8fcf8ea424527b70d08f9613098
Maickiplay <mssanchez908@misena.edu.co>
2019-04-26 12:28:38 -0500

    fgfgfs

--- golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
+++ golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
@@ -55,35 +55,95 @@
                               </v-btn>
                               </v-flex>
                           </v-layout>
-                            <v-data-table
-                              :headers="headers"
-                              :items="desserts"
-                              class="elevation-1"
-                            >
-                              <template v-slot:items="props">
-                                <td>{{ props.item.name }}</td>
-                                <td class="text-xs-right">{{ props.item.calories }}</td>
-                                <td class="text-xs-right">{{ props.item.fat }}</td>
-                                <td class="text-xs-right">{{ props.item.carbs }}</td>
-                                <td class="text-xs-right">{{ props.item.protein }}</td>
-
-                                <td class="justify-center layout px-0">
-                                  <v-icon
-                                    small
-                                    class="mr-2"
-                                    @click="editItem(props.item)"
-                                  >
-                                    edit
-                                  </v-icon>
-                                  <v-icon
-                                    small
-                                    @click="deleteItem(props.item)"
-                                  >
-                                    delete
-                                  </v-icon>
-                                </td>
-                              </template>
-                            </v-data-table>
+                            <div id="app">
+  <v-app id="inspire">
+    <v-card>
+      <v-card-title class="indigo white--text headline">
+        User Directory
+      </v-card-title>
+      <v-layout
+        justify-space-between
+        pa-3
+      >
+        <v-flex xs5>
+          <v-treeview
+            :active.sync="active"
+            :items="items"
+            :load-children="fetchUsers"
+            :open.sync="open"
+            activatable
+            active-class="primary--text"
+            class="grey lighten-5"
+            open-on-click
+            transition
+          >
+            <template v-slot:prepend="{ item, active }">
+              <v-icon
+                v-if="!item.children"
+                :color="active ? 'primary' : ''"
+              >
+                mdi-account
+              </v-icon>
+            </template>
+          </v-treeview>
+        </v-flex>
+        <v-flex
+          d-flex
+          text-xs-center
+        >
+          <v-scroll-y-transition mode="out-in">
+            <div
+              v-if="!selected"
+              class="title grey--text text--lighten-1 font-weight-light"
+              style="align-self: center;"
+            >
+              Select a User
+            </div>
+            <v-card
+              v-else
+              :key="selected.id"
+              class="pt-4 mx-auto"
+              flat
+              max-width="400"
+            >
+              <v-card-text>
+                <v-avatar
+                  v-if="avatar"
+                  size="88"
+                >
+                  <v-img
+                    :src="`https://avataaars.io/${avatar}`"
+                    class="mb-4"
+                  ></v-img>
+                </v-avatar>
+                <h3 class="headline mb-2">
+                  {{ selected.name }}
+                </h3>
+                <div class="blue--text mb-2">{{ selected.email }}</div>
+                <div class="blue--text subheading font-weight-bold">{{ selected.username }}</div>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-layout
+                tag="v-card-text"
+                text-xs-left
+                wrap
+              >
+                <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Company:</v-flex>
+                <v-flex>{{ selected.company.name }}</v-flex>
+                <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Website:</v-flex>
+                <v-flex>
+                  <a :href="`//${selected.website}`" target="_blank">{{ selected.website }}</a>
+                </v-flex>
+                <v-flex tag="strong" xs5 text-xs-right mr-3 mb-2>Phone:</v-flex>
+                <v-flex>{{ selected.phone }}</v-flex>
+              </v-layout>
+            </v-card>
+          </v-scroll-y-transition>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-app>
+</div>
                       </v-container>
                   </v-form>
               </material-card>
@@ -105,57 +165,6 @@ export default {
       rules: {
         required: value => !!value || 'Requirido',
         min: v => v.length >= 8 || 'Minimo 8 caracteres'
-
-      },
-      headers: [
-        {
-          text: 'Nombre',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Usuario', value: 'calories', align: 'center' },
-        { text: 'Identificación', value: 'fat', align: 'center' },
-        { text: 'Correo', value: 'carbs', align: 'center' },
-        { text: 'Telefono', value: 'protein', align: 'center' },
-        { text: 'Acciones', value: 'iron' }
-      ],
-      desserts: [
-        {
-          name: 'Maicol Sánchez',
-          calories: 'Maicky',
-          fat: 10456456,
-          carbs: 'mssanchez@gmail.com',
-          protein: 345435345
-        },
-        {
-          name: 'Maicol Sánchez',
-          calories: 'Maicky',
-          fat: 10456456,
-          carbs: 'mssanchez@gmail.com',
-          protein: 345435345
-        }
-      ],
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Estas seguro que quieres eliminar este item?') && this.desserts.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
       }
 
     }
