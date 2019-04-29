23f80a879e835f9bcb3205bcfd441b3a7f27541b
Maickiplay <mssanchez908@misena.edu.co>
2019-04-29 08:04:04 -0500

    vdfvdfv

--- golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
+++ golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
@@ -153,7 +153,16 @@
   </div>
 </template>
 
-<script scoped>
+<script>
+const avatars = [
+    '?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban',
+    '?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun',
+    '?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong',
+    '?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair',
+    '?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly'
+  ]
+
+  const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
 export default {
   data () {
     return {
@@ -165,13 +174,54 @@ export default {
       rules: {
         required: value => !!value || 'Requirido',
         min: v => v.length >= 8 || 'Minimo 8 caracteres'
-      }
+      },
+      active: [],
+      avatar: null,
+      open: [],
+      users: []
 
     }
   },
   created () {
     this.$store.commit('SET_LAYOUT', 'administrador-layout')
-  }
+  },
+  computed: {
+      items () {
+        return [
+          {
+            name: 'Users',
+            children: this.users
+          }
+        ]
+      },
+      selected () {
+        if (!this.active.length) return undefined
+
+        const id = this.active[0]
+
+        return this.users.find(user => user.id === id)
+      }
+    },
+
+    watch: {
+      selected: 'randomAvatar'
+    },
+
+    methods: {
+      async fetchUsers (item) {
+        // Remove in 6 months and say
+        // you've made optimizations! :)
+        await pause(1500)
+
+        return fetch('https://jsonplaceholder.typicode.com/users')
+          .then(res => res.json())
+          .then(json => (item.children.push(...json)))
+          .catch(err => console.warn(err))
+      },
+      randomAvatar () {
+        this.avatar = avatars[Math.floor(Math.random() * avatars.length)]
+      }
+    }
 }
 </script>
 
