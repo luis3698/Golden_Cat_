19d00381eee3662c393043370c367fa93def170a
CamiloCarvajal99 <jccarvajal043@misena.edu.co>
2019-05-02 13:59:06 -0500

    Actualizacion

--- golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
+++ golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
@@ -202,18 +202,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-const avatars = [
-  '?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban',
-  '?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun',
-  '?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong',
-  '?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair',
-  '?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly'
-]
-
-const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
-=======
->>>>>>> c7f6fae7f07477b9eb0d3056fd12c5fff73aaec3
 export default {
   created () {
     this.$store.commit('SET_LAYOUT', 'administrador-layout')
@@ -276,45 +264,6 @@ export default {
   created () {
     this.initialize()
   },
-<<<<<<< HEAD
-  computed: {
-    items () {
-      return [
-        {
-          name: 'Users',
-          children: this.users
-        }
-      ]
-    },
-    selected () {
-      if (!this.active.length) return undefined
-
-      const id = this.active[0]
-
-      return this.users.find(user => user.id === id)
-    }
-  },
-
-  watch: {
-    selected: 'randomAvatar'
-  },
-
-  methods: {
-    async fetchUsers (item) {
-      // Remove in 6 months and say
-      // you've made optimizations! :)
-      await pause(1500)
-
-      return fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(json => (item.children.push(...json)))
-        .catch(err => console.warn(err))
-    },
-    randomAvatar () {
-      this.avatar = avatars[Math.floor(Math.random() * avatars.length)]
-    }
-  }
-=======
   methods: {
     initialize () {
       this.desserts = [
@@ -394,7 +343,6 @@ export default {
     }
   }
 
->>>>>>> c7f6fae7f07477b9eb0d3056fd12c5fff73aaec3
 }
 </script>
 
