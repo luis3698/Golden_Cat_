3cf102e54c05ec4a26a7c7aa5fd06bbc8e0cc2cd
Aprendiz <Aprendiz@SGLDFPCAT7F572.FormacionSena.RED>
2019-05-03 12:52:51 -0500

    base de datosssss

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,12 +3,10 @@
 const { getLogger } = require('@golden-cat/utils')
 const user = require('./user')
 const room = require('./room')
-<<<<<<< HEAD
+
 const type_room = require('./type_room')
-=======
 const rate = require('./rate')
 const season = require('./season')
->>>>>>> 80b8081093804d6f97b32b035f6ba1f883999b07
 
 const log = getLogger(__dirname, __filename)
 // prueba
@@ -20,12 +18,10 @@ module.exports = {
   },
    user,
    room,
-<<<<<<< HEAD
    type_room,
-=======
    rate,
    season,
->>>>>>> 80b8081093804d6f97b32b035f6ba1f883999b07
+
   errorHandler (err, req, res, next) {
     if (err) {
       let code = err.code || 500
