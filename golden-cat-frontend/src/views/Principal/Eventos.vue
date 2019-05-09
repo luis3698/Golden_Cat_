a334674351262d702a4127171044fbd78f768fe2
luis3698 <luisgerardo3698@gmail.com>
2019-05-09 15:57:32 -0500

    jjj

--- golden-cat-frontend/src/views/Principal/Eventos.vue
+++ golden-cat-frontend/src/views/Principal/Eventos.vue
@@ -13,9 +13,45 @@
         </v-flex>
       </v-layout>
       <v-layout row>
-        <v-flex xs4 order-md2 >
-          <v-card dark tile
-          
+        <v-flex xs4  >
+          <v-card dark tile flat color="red lighten-1">
+            <v-card-text>#1</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs4 >
+          <v-card dark tile flat color="red lighten-2">
+            <v-card-text>#2</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs4 >
+          <v-card dark tile flat color="red darken-1">
+            <v-card-text>#3</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md3  >
+          <v-card dark tile flat color="red darken-2">
+            <v-card-text>#1</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm6 md3 >
+          <v-card dark tile flat color="deep-orange lighten-1">
+            <v-card-text>#2</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm6 md3 >
+          <v-card dark tile flat color="deep-orange darken-3">
+            <v-card-text>#3</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm6 md3>
+          <v-card dark tile flat color="deep-orange">
+            <v-card-text>#4</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
 </script>
 <style>
 
