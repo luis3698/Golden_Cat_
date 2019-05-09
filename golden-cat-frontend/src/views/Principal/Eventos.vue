1b25dbc32b5a08bad172d8b4e00db83fe020d32f
luis3698 <luisgerardo3698@gmail.com>
2019-05-09 14:47:59 -0500

    actual

--- golden-cat-frontend/src/views/Principal/Eventos.vue
+++ golden-cat-frontend/src/views/Principal/Eventos.vue
@@ -1,11 +1,22 @@
 <template>
-
+<v-container class="contenedor" fluid>
+<v-img class="imgportada" :src="mesero" ></v-img>
+</v-container>
 </template>
 <script>
-export default {
-
-}
+import MESERO from '@/assets/mesero.jpg'
+export default ({
+  data () {
+    return {
+      mesero: MESERO
+    }
+  }
+})
 </script>
-<style>
-
+<style scoped>
+.imgportada{
+    height:300px;
+    width:100%;
+}
 </style>
+
