d2b402c4726e7a4b2371abfae992d736a6f54e67
Maickiplay <mssanchez908@misena.edu.co>
2019-05-28 12:46:46 -0500

    dkjashka

--- golden-cat-frontend/src/views/Administrador/Realizarreserva.vue
+++ golden-cat-frontend/src/views/Administrador/Realizarreserva.vue
@@ -104,6 +104,13 @@
                 ></v-text-field>
               </v-flex>
               <v-flex xs12 sm6>
+                <v-select
+                  :items="items"
+                  label="Tipo habitacion"
+                  required
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm6>
                 <v-autocomplete
                   label="Departamento"
                   v-model="departamento"
@@ -132,7 +139,8 @@
                 ></v-select>
               </v-flex>
               <v-flex xs12 sm6>
-                <v-text-field label="Password*" type="password" required></v-text-field>
+                <v-text-field  hint="Solo en caso de pago con tarjeta"
+                  persistent-hint label="Password*" type="password" required></v-text-field>
               </v-flex>
               
             </v-layout>
@@ -152,14 +160,78 @@
 
             <v-flex xs12>
     <div class="rojo">
-      <v-tabs v-model="active" color="blue darken-1" dark slider-color="greed" >
+      <v-tabs v-model="active"  color="blue darken-1" dark slider-color="greed" >
         <v-tab ripple> reservas activas</v-tab>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text color="green" >
+              <template v-for="n in 8">
+              <v-flex xs12 :key="n" mb-3>
+                <v-card color="blue darken-1"  class="white--text" >
+                  <v-layout row>
+                    <v-flex xs4>
+                      <v-card-title primary-title>
+                        <div>
+                          <div class="headline mb-4">tipo de habitacion: doble</div>
+                          <div  class="mb-4">fecha de la reserva  : 25/05/2017 </div>
+                          <div  class=" mb-4">numero de adultos : 2</div>
+                          <div  class=" mb-4">tipo de habitacion: estandar</div>
+                        </div>
+                      </v-card-title>
+                    </v-flex>
+                    <v-flex xs4>
+                      <v-card-title primary-title>
+                        <div>
+
+                          <div class="headline mb-4">precio de la habitacion </div>
+                          <div  class="title mb-4 ">650000$ </div>
+                          <div>numero de niños: 0</div>
+
+                        </div>
+
+                      </v-card-title>
+                    </v-flex>
+                    <v-flex xs4>
+                      <v-img :src="habitacion2" contain></v-img>
+                    </v-flex>
+                  </v-layout>
+                  <v-divider light></v-divider>
+                  <v-expansion-panel>
+                        <v-expansion-panel-content >
+                          <template v-slot:header>
+                            <div>detalles</div>
+                          </template>
+                          <v-card>
+                            <v-card-text>
+                              *Aire acondicionado <br>
+                              *Baño con ducha, secador de pelo y servicio de *amenidades diarias<br>
+                              *Caja de seguridad electrónica<br>
+                              * Voltaje (corriente): 220 V/60 Hz (los baños cuentan con tomas de 110 V para equipos hasta 50 W)<br>
+                              *Minibar con agua, refrescos, cervezas y más ($)<br>
+                              *Teléfono directo en dormitorio y baño<br>
+                              *1 cama extra o una cuna (a solicitud)<br>
+                              *2 Baños
+
+                            </v-card-text>
+                            <v-btn small color="blue darken-1"  >borrar reserva<v-icon small>business</v-icon>
+                            </v-btn>
+                          </v-card>
+                        </v-expansion-panel-content>
+                      </v-expansion-panel>
+                </v-card>
+              </v-flex>
+              </template >
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab ripple> reservas en curso</v-tab>
         <v-tab-item>
           <v-card flat>
             <v-card-text color="green">
               <template v-for="n in 8">
               <v-flex xs12 :key="n" mb-3>
-                <v-card color="red" class="blue darken-1">
+                <v-card color="green darken-1" class="white--text">
                   <v-layout row>
                     <v-flex xs4>
                       <v-card-title primary-title>
@@ -222,7 +294,7 @@
           <v-card flat>
             <v-card-text> <template v-for="n in 8">
               <v-flex xs12 :key="n" mb-3>
-                <v-card color="orange darken-4" class="white--text">
+                <v-card color="red darken-1" class="white--text">
                   <v-layout row >
                     <v-flex xs4>
                       <v-card-title primary-title>
