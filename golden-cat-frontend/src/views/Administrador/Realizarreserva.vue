185f9b1669c2a78741f0e583dffe6eb0e96b5ad9
Maickiplay <mssanchez908@misena.edu.co>
2019-05-30 12:48:02 -0500

    fdffd

--- golden-cat-frontend/src/views/Administrador/Realizarreserva.vue
+++ golden-cat-frontend/src/views/Administrador/Realizarreserva.vue
@@ -76,13 +76,14 @@
           <v-container grid-list-md>
             <v-layout wrap>
               <v-flex xs12 sm6 md4>
-                <v-text-field label="Nombres" required></v-text-field>
+                <v-text-field label="Nombres" v-model="nombre" required></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
-                <v-text-field label="Apellidos"></v-text-field>
+                <v-text-field label="Apellidos" v-model="apellido"></v-text-field>
               </v-flex>
               <v-flex xs12 sm6 md4>
                 <v-text-field
+                v-model="id"
                   label="Identificación"
                   hint="Solo mayor de edad"
                   persistent-hint
@@ -91,6 +92,7 @@
               </v-flex>
               <v-flex xs6>
                 <v-text-field
+                v-model="email"
                  label="Email*" 
                  required>
                  </v-text-field>
@@ -110,39 +112,43 @@
                   required
                 ></v-select>
               </v-flex>
-
-
               <v-flex xs12 sm6>
-        <v-text-field background-color="white" color="white" dark="false" solo class="fecha,todo" label="seleccionar fechas" ref="myinput"  v-model="range"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm12 class="todo">
-        {{date}}
-      </v-flex> 
-
-
-
-
-
+                <v-text-field background-color="white" color="white" dark="false" regular class="fecha,todo" label="seleccionar fechas" ref="myinput"  v-model="range"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm12 class="todo">
+                {{date}}
+              </v-flex>
               <v-flex xs12 sm6>
-                <v-autocomplete
+                <v-text-field type="number" v-model="niños" label="No. niños"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-text-field type="number" v-model="adultos" label="No. adultos"></v-text-field>
+              </v-flex> 
+              <v-flex xs12 sm4>
+                <v-select
                   label="Departamento"
                   v-model="departamento"
                   :items="departamentos"
                   hint="Solo en caso de provenir del mismo pais"
-                  persistent-hint
-                  multiple
-                ></v-autocomplete>
+                  persistent-hintDF
+                ></v-select>
               </v-flex>
-              <v-flex xs12 sm6>
-                <v-autocomplete
+              <v-flex xs12 sm4>
+                <v-select
                   ref="country"
                   v-model="country"
                   :items="countries"
                   label="Pais"
                   hint="Solo en caso de provenir del exterior"
                   persistent-hint
-                  multiple
-                ></v-autocomplete>
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  v-model="precio"
+                  label="Precio"
+                  required
+                ></v-text-field>
               </v-flex>
               <v-flex xs12 sm6>
                 <v-select
@@ -183,37 +189,30 @@
                 <v-card color="blue darken-1"  class="white--text" >
                   <v-layout row>
                     <v-flex xs5>
-                      <v-card-title primary-title>
+                      <v-card-title>
                         <div>
-                          <h2 class=" mb-4">Nombres: {{ tipo }}</h2>
-                          
-                          <h2 class=" mb-4">identificacion: {{ tipo }}</h2>
-                          <h2 class=" mb-4">Telefono: {{ tipo }}</h2>
-                          
-                          <h2  class="mb-4">fecha de la reserva  : 25/05/2017 </h2>
-                          <h2  class=" mb-4">numero de adultos : 2</h2>
+                          <h4 class=" mb-3">Nombres: {{ nombre }}</h4>
+                          <h4 class=" mb-3">identificacion: {{ id }}</h4>
+                          <h4 class=" mb-3">Telefono: {{ tel }}</h4>
+                          <h4 class=" mb-3">fecha de la reserva: {{ range }}</h4>
+                          <h4 class=" mb-3">Pais: {{ country }}</h4>
+                          <h4 class=" mb-3">numero de adultos: {{ adultos }} </h4>
                         </div>
                       </v-card-title>
                     </v-flex>
                     <v-flex xs4>
-                      <v-card-title primary-title>
+                      <v-card-title>
                         <div>
-                          <h2 class=" mb-4">Apellidos: {{ tipo }}</h2>
-                          <h2 class=" mb-4">Email: {{ tipo }}</h2>
-                          <h2 class=" mb-4">tipo de habitacion: {{ tipo }}</h2>
-                          <h2 class=" mb-4">precio de la habitacion </h2>
-                          <h2  class="mb-4 ">650000$ </h2>
-                          <h2>numero de niños: 0</h2>
-
+                          <h4 class=" mb-3">Apellidos: {{ apellido }}</h4>
+                          <h4 class=" mb-3">Email: {{ email }}</h4>
+                          <h4 class=" mb-3">tipo de habitacion: {{ tipo }}</h4>
+                          <h4 class=" mb-3">Departamento: {{ departamento }}</h4>
+                          <h4 class=" mb-3">precio de la habitacion: {{ precio }}</h4>
+                          <h4>numero de niños: {{ niños }}</h4>
                         </div>
-
                       </v-card-title>
-                    </v-flex>
-                    <v-flex xs4>
-                      <v-img :src="habitacion2" contain></v-img>
                     </v-flex>
                   </v-layout>
-                  <v-divider light></v-divider>
                 </v-card>
               </v-flex>
               </template >
@@ -347,6 +346,9 @@
    </v-flex>
 </template>
 <script>
+import { Spanish } from 'flatpickr/dist/l10n/es.js'
+import 'flatpickr/dist/flatpickr.css'
+import 'flatpickr/dist/themes/material_blue.css'
 export default {
   data: () => ({
    date: new Date().toISOString().substr(0, 10),
@@ -366,8 +368,15 @@ export default {
     this.$store.commit('SET_LAYOUT', 'administrador-layout')
     this.initialize()
   },
+    mounted () {
+    const myInput = this.$refs.myinput.$el.querySelector('input')
+    const flat = flatpickr(myInput, this.configFlat)
+  },
   data: () => ({
     carta: [],
+    date: '',
+    configFlat: { mode: 'range', dateFormat: 'Y-m-d H:i', locale: Spanish },
+    range: '',
     countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad  Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
     items: ['Sencilla', 'Doble', 'Swite', 'Presidencial'],
     dialog: false,
