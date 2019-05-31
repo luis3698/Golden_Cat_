43e5661726d360c6666812509989c77dd6d94586
sebas2125 <jsmancillla@misena.edu.co>
2019-05-31 14:53:48 -0500

    formulario

--- golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
+++ golden-cat-frontend/src/views/Administrador/AdministrarUsuarios.vue
@@ -1,12 +1,6 @@
 <template>
-    <v-card class="este" flat>
-    <v-snackbar
-      v-model="snackbar"
-      absolute
-      top
-      right
-      color="success"
-    >
+  <v-card class="este" flat>
+    <v-snackbar v-model="snackbar" absolute top right color="success">
       <span>Registration successful!</span>
       <v-icon dark>check_circle</v-icon>
     </v-snackbar>
@@ -15,55 +9,86 @@
         <v-layout wrap>
           <v-flex xs12 sm6>
             <v-text-field
-              v-model="form.first"
+              v-model="form.nombre"
               :rules="rules.name"
               color="purple darken-2"
-              label="First name"
+              label="Nombres"
               required
             ></v-text-field>
           </v-flex>
           <v-flex xs12 sm6>
             <v-text-field
-              v-model="form.last"
+              v-model="form.apellidos"
               :rules="rules.name"
               color="blue darken-2"
-              label="Last name"
+              label="Apellidos"
               required
             ></v-text-field>
           </v-flex>
-          <v-flex xs12>
-            <v-textarea
-              v-model="form.bio"
-              color="teal"
-            >
-              <template v-slot:label>
-                <div>
-                  Bio <small>(optional)</small>
-                </div>
-              </template>
-            </v-textarea>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="form.correo"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Correo Electronico"
+              required
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="form.telefono"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Telefono"
+              required
+              type="number"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="form.id"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Indentificacion"
+              required
+              type="number"
+            ></v-text-field>
           </v-flex>
           <v-flex xs12 sm6>
             <v-select
-              v-model="form.favoriteAnimal"
-              :items="animals"
-              :rules="rules.animal"
-              color="pink"
-              label="Favorite animal"
+              v-model="form.usuario"
+              :items= "usuario"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Tipo Usuario"
               required
             ></v-select>
           </v-flex>
           <v-flex xs12 sm6>
-            <v-slider
-              v-model="form.age"
-              :rules="rules.age"
-              color="orange"
-              label="Age"
-              hint="Be honest"
-              min="1"
-              max="100"
-              thumb-label
-            ></v-slider>
+            <v-text-field
+              v-model="form.contraseña"
+              :append-icon="show1 ? 'visibility' : 'visibility_off'"
+              :type="show1 ? 'text' : 'password'"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Contraseña"
+              required
+              @click:append="show1 = !show1"
+              class="input-group--focused"
+            ></v-text-field>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-text-field
+              v-model="form.Ccontraseña"
+              :append-icon="show2 ? 'visibility' : 'visibility_off'"
+              :type="show2 ? 'text' : 'password'"
+              :rules="rules.name"
+              color="blue darken-2"
+              label="Confirmar Contraseña"
+              required
+              @click:append="show2 = !show2"
+              class="input-group--focused"
+            ></v-text-field>
           </v-flex>
           <v-flex xs12>
             <v-checkbox
@@ -72,10 +97,10 @@
             >
               <template v-slot:label>
                 <div @click.stop="">
-                  Do you accept the
-                  <a href="javascript:;" @click.stop="terms = true">terms</a>
-                  and
-                  <a href="javascript:;" @click.stop="conditions = true">conditions?</a>
+                  Aceptas los 
+                  <a href="javascript:;" @click.stop="terms = true">terminos</a>
+                  y
+                  <a href="javascript:;" @click.stop="conditions = true">condiciones</a>?
                 </div>
               </template>
             </v-checkbox>
@@ -144,26 +169,27 @@ export default {
   // },
   data () {
     const defaultForm = Object.freeze({
-      first: '',
-      last: '',
-      bio: '',
-      favoriteAnimal: '',
-      age: null,
+      nombre: '',
+      apellidos: '',
+      correo: '',
+      telefono: '',
+      id: '',
+      usuario: '',
+      contraseña: '',
+      Ccontraseña: '',
       terms: false
     })
 
     return {
+      show1: false,
+      show2: false,
       active: null,
       form: Object.assign({}, defaultForm),
       rules: {
-        age: [
-          val => val < 10 || `I don't believe you!`
-        ],
-        animal: [val => (val || '').length > 0 || 'This field is required'],
         name: [val => (val || '').length > 0 || 'This field is required']
       },
-      animals: ['Dog', 'Cat', 'Rabbit', 'Turtle', 'Snake'],
       conditions: false,
+      usuario: ['Cliente', 'Administrador'],
       content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.`,
       snackbar: false,
       terms: false,
@@ -174,10 +200,15 @@ export default {
   computed: {
     formIsValid () {
       return (
-        this.form.first &&
-          this.form.last &&
-          this.form.favoriteAnimal &&
-          this.form.terms
+        this.form.nombre &&
+        this.form.apellidos &&
+        this.form.correo &&
+        this.form.telefono &&
+        this.form.id &&
+        this.form.usuario &&
+        this.form.contraseña &&
+        this.form.Ccontraseña &&
+        this.form.terms
       )
     }
   },
